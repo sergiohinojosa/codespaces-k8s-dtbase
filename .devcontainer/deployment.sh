93fde1a525240135be56fcc833f54ef9319ceb7a
kyledharrington <kyleduganharrington@gmail.com>
2024-09-25 08:27:47 -0700

    stripping Dynatrace deployment objects

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -2,10 +2,10 @@
 
 ### delete me
 #kubectl create namespace log-generator
-kubectl create namespace dynatrace
+# kubectl create namespace dynatrace
 
-sed -i "s,TENANTURL_TOREPLACE,$DT_URL," /workspaces/$RepositoryName/dynatrace/dynakube.yaml
-sed -i "s,CLUSTER_NAME_TO_REPLACE,github-codespace-cluster,"  /workspaces/$RepositoryName/dynatrace/dynakube.yaml
+# sed -i "s,TENANTURL_TOREPLACE,$DT_URL," /workspaces/$RepositoryName/dynatrace/dynakube.yaml
+# sed -i "s,CLUSTER_NAME_TO_REPLACE,github-codespace-cluster,"  /workspaces/$RepositoryName/dynatrace/dynakube.yaml
 
 ### delete me ?
 #clusterName=`kubectl config view --minify -o jsonpath='{.clusters[].name}'`
@@ -14,7 +14,7 @@ sed -i "s,CLUSTER_NAME_TO_REPLACE,github-codespace-cluster,"  /workspaces/$Repos
 
 ### delete me 
 #sed -i "s,{YOUR_DT_URL},$DT_URL,"  /workspaces/$RepositoryName/deployment/LogGenerator.yaml
-sed -i "s,{YOUR_DT_LOG_INGEST_TOKEN},$DT_LOG_INGEST_TOKEN,"  /workspaces/$RepositoryName/deployment/LogGenerator.yaml
+#sed -i "s,{YOUR_DT_LOG_INGEST_TOKEN},$DT_LOG_INGEST_TOKEN,"  /workspaces/$RepositoryName/deployment/LogGenerator.yaml
 #Extract the tenant name from DT_URL variable
 #tenantId=`echo $DT_URL | awk -F "[/]" '{print $3}'`
 #sed -i "s,{ENTER_YOUR_TENANT_ID},$tenantId,"  /workspaces/$RepositoryName/dynatrace/values.yaml
@@ -26,18 +26,18 @@ sed -i "s,{YOUR_DT_LOG_INGEST_TOKEN},$DT_LOG_INGEST_TOKEN,"  /workspaces/$Reposi
 #   --from-literal=DT_API_TOKEN=$DT_OPERATOR_TOKEN
 
 # Deploy Dynatrace
-kubectl -n dynatrace create secret generic dynakube --from-literal="apiToken=$DT_OPERATOR_TOKEN" --from-literal="dataIngestToken=$DT_DATAINGEST_TOKEN"
+# kubectl -n dynatrace create secret generic dynakube --from-literal="apiToken=$DT_OPERATOR_TOKEN" --from-literal="dataIngestToken=$DT_DATAINGEST_TOKEN"
 
-wget -O /workspaces/$RepositoryName/dynatrace/kubernetes.yaml https://github.com/Dynatrace/dynatrace-operator/releases/latest/download/kubernetes.yaml                                                           
-wget -O /workspaces/$RepositoryName/dynatrace/kubernetes-csi.yaml https://github.com/Dynatrace/dynatrace-operator/releases/latest/download/kubernetes-csi.yaml
-sed -i "s,cpu: 300m,cpu: 100m," /workspaces/$RepositoryName/dynatrace/kubernetes.yaml
-sed -i "s,cpu: 300m,cpu: 100m," /workspaces/$RepositoryName/dynatrace/kubernetes-csi.yaml
-# Shrink resource utilisation to work on GitHub codespaces (ie. a small environment)
-# Apply (slightly) customised manifests
-kubectl apply -f /workspaces/$RepositoryName/dynatrace/kubernetes.yaml
-kubectl apply -f /workspaces/$RepositoryName/dynatrace/kubernetes-csi.yaml
-kubectl -n dynatrace wait pod --for=condition=ready --selector=app.kubernetes.io/name=dynatrace-operator,app.kubernetes.io/component=webhook --timeout=300s
-kubectl -n dynatrace apply -f /workspaces/$RepositoryName/dynatrace/dynakube.yaml
+# wget -O /workspaces/$RepositoryName/dynatrace/kubernetes.yaml https://github.com/Dynatrace/dynatrace-operator/releases/latest/download/kubernetes.yaml                                                           
+# wget -O /workspaces/$RepositoryName/dynatrace/kubernetes-csi.yaml https://github.com/Dynatrace/dynatrace-operator/releases/latest/download/kubernetes-csi.yaml
+# sed -i "s,cpu: 300m,cpu: 100m," /workspaces/$RepositoryName/dynatrace/kubernetes.yaml
+# sed -i "s,cpu: 300m,cpu: 100m," /workspaces/$RepositoryName/dynatrace/kubernetes-csi.yaml
+# # Shrink resource utilisation to work on GitHub codespaces (ie. a small environment)
+# # Apply (slightly) customised manifests
+# kubectl apply -f /workspaces/$RepositoryName/dynatrace/kubernetes.yaml
+# kubectl apply -f /workspaces/$RepositoryName/dynatrace/kubernetes-csi.yaml
+# kubectl -n dynatrace wait pod --for=condition=ready --selector=app.kubernetes.io/name=dynatrace-operator,app.kubernetes.io/component=webhook --timeout=300s
+# kubectl -n dynatrace apply -f /workspaces/$RepositoryName/dynatrace/dynakube.yaml
 
 #kubectl create secret generic dynatrace-otelcol-dt-api-credentials \
 #  --from-literal=DT_ENDPOINT=$DT_URL \
@@ -56,11 +56,11 @@ kubectl -n dynatrace apply -f /workspaces/$RepositoryName/dynatrace/dynakube.yam
 #kubectl apply -f deployment/LogGenerator.yaml -n log-generator
 
 # Wait for Dynatrace to be ready
-kubectl -n dynatrace wait --for=condition=Ready pod --all --timeout=10m
+#kubectl -n dynatrace wait --for=condition=Ready pod --all --timeout=10m
 
 ### delete me
 # Wait for travel advisor system to be ready
 #kubectl -n log-generator wait --for=condition=Ready pod --all --timeout=10m
 
 ### Deploy dt-boutique
-kubectl apply -f https://raw.githubusercontent.com/kyledharrington/dt-boutique/main/kind-dt-boutique.yaml -n dt-boutique
+#kubectl apply -f https://raw.githubusercontent.com/kyledharrington/dt-boutique/main/kind-dt-boutique.yaml -n dt-boutique
