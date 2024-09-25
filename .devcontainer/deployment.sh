857d2f2ba7b480bf815b0b6290d1cf84ccb02c49
kyledharrington <kyleduganharrington@gmail.com>
2024-09-25 15:41:28 -0700

    overhauling

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+NAME=$(whoami)-k8s-otel-o11y   
+
 # #istio setup
 export PATH=$PWD/istio-1.22.1/bin:$PATH
 istioctl install -f istio/istio-operator.yaml --skip-confirmation
+
+sleep 30 
+##########################
+# update istio ingress
+kubectl patch svc -n istio-system istio-ingressgateway --patch "$(cat deployment/patch.yaml)"
+kubectl delete pod --all -n istio-system   
+
+### 
+# deploy astronomy shop
+sed -i "s,NAME_TO_REPLACE,$NAME," astronomy-shop/default-values.yaml
+helm install astronomy-shop open-telemetry/opentelemetry-demo --values astronomy-shop/default-values.yaml --namespace astronomy-shop --version "0.31.0"
 
 ### delete me
 #kubectl create namespace log-generator
